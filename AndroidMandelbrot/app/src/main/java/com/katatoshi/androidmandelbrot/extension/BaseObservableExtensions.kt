ce086c4e6f683180894ee9cf781f1ea89af8ab17
katatoshi <kata.toshi@gmail.com>
2017-02-19 04:04:49 +0900

    BaseObservable の OnPropertyChangedCallback としてラムダを渡せるような拡張を追加

---
+++ AndroidMandelbrot/app/src/main/java/com/katatoshi/androidmandelbrot/extension/BaseObservableExtensions.kt
@@ -0,0 +1,33 @@
+package com.katatoshi.androidmandelbrot.extension
+
+import android.databinding.BaseObservable
+import android.databinding.Observable
+
+/**
+ * BaseObservable に対する拡張
+ */
+
+private var callbackMap: MutableMap<(Observable, Int) -> Unit, Observable.OnPropertyChangedCallback> = mutableMapOf()
+
+fun BaseObservable.addOnPropertyChangedCallback(callback: (Observable, Int) -> Unit) {
+    if (callbackMap.containsKey(callback)) {
+        return
+    }
+
+    val onPropertyChangedCallback = object : Observable.OnPropertyChangedCallback() {
+        override fun onPropertyChanged(sender: Observable, propertyId: Int) {
+            callback(sender, propertyId)
+        }
+    }
+    callbackMap[callback] = onPropertyChangedCallback
+    this.addOnPropertyChangedCallback(onPropertyChangedCallback)
+}
+
+fun BaseObservable.removeOnPropertyChangedCallback(callback: (Observable, Int) -> Unit) {
+    if (!callbackMap.containsKey(callback)) {
+        return
+    }
+
+    this.removeOnPropertyChangedCallback(callbackMap[callback])
+    callbackMap.remove(callback)
+}
