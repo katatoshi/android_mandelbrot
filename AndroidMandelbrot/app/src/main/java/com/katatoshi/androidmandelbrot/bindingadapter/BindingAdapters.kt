d05ac43a098a2885c3e05bf9d84252f77f085ceb
katatoshi <kata.toshi@gmail.com>
2017-02-21 08:53:17 +0900

    refactoring: コメント修正

--- AndroidMandelbrot/app/src/main/java/com/katatoshi/androidmandelbrot/bindingadapter/BindingAdapters.kt
+++ AndroidMandelbrot/app/src/main/java/com/katatoshi/androidmandelbrot/bindingadapter/BindingAdapters.kt
@@ -4,8 +4,10 @@ import android.databinding.BindingAdapter
 import android.view.View
 
 /**
- * カスタムセッター
- * Java に変換したときに static メソッドになる必要があるので、トップレベルの関数として定義する
+ * カスタムセッター。
+ * Java に変換したときに static メソッドになる必要があるので、トップレベルの関数として定義する。
+ * カスタムセッターは XML から定義に飛べず、しかも重複して定義できるので（重複して定義したときにどれが優先されるかはわからない）一箇所にまとめておく。
+ * カスタム View に対するカスタムセッターもこちらに置くべきかは考え中。
  */
 
 @BindingAdapter("invisible")
